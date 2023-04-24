87b6f2b963b896d2afc8d17c4c52e35c3884d2e0
mecene <mecene.co@gmail.com>
2023-04-24 13:17:10 +0200

    n

--- mc_productdatechanger.php
+++ mc_productdatechanger.php
@@ -219,7 +219,7 @@ Reorganise your product.');
         //return $params['id_product'];
         $this->context->smarty->assign([
             'product_id' => $params['id_product'],
-            'product_date' => $params['_content']['date_add'],
+            'product_date' => $params["_content"],
             'params' => $params
         ]);
         return $this->display(__FILE__, 'views/templates/admin/datepicker.tpl');
--- views/templates/admin/datepicker.tpl
+++ views/templates/admin/datepicker.tpl
@@ -7,5 +7,5 @@
         {l s='Creation date.' mod='mc_productdatechanger'} : {$product_date}<br />
 	</p>
 </div>
-{$params|@var_dump}
+{$product_date|@var_dump}
 {* {$var|@var_dump} *}
